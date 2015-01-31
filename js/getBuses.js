5c8157ec552c2121d2e62b1a0d1efc8a87bcbac2
Matt Goldman <robotmlg@gmail.com>
2015-01-31 18:57:41 -0500

    script skeleton

--- js/getBuses.js
+++ js/getBuses.js
@@ -1,5 +1,12 @@
 //pulls NextBus data for the appropriate buses
 
+//init
 $(document).ready(function() {
   console.log( 'ready!' );
 });
+
+//get EE bus data
+
+//if there is no EE bus data, get weekend bus data
+
+//put data on screen
